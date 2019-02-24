2e71cf5164ecae3ee23951430683ad880be4fcfd
Marck C. Guzmán <marck.guzman.dev@gmail.com>
2019-02-24 14:30:14 +0100

    renamed the LatLong attributes
    Signed-off-by: Marck C. Guzmán <marck.guzman.dev@gmail.com>

--- LatLong.py
+++ LatLong.py
@@ -4,30 +4,30 @@ class LatLong:
     Representate a coordenate position
     """
     def __init__(self, latitude, longitude):
-        self.latitude = latitude;
-        self.longitude = longitude;
+        self.__latitude__ = latitude;
+        self.__longitude__ = longitude;
 
     def get_lat(self):
         """
 
         :return: the latitude
         """
-        return self.latitude
+        return self.__latitude__
 
     def get_long(self):
         """
 
         :return: the longitude
         """
-        return self.longitude
+        return self.__longitude__
 
     def __str__(self):
-        return "({},{})".format(self.latitude, self.longitude)
+        return "({},{})".format(self.__latitude__, self.__longitude__)
 
     def __add__(self, other):
         if type(other) != type(self):
             return;
         else:
-            lng = self.longitude + other.get_long()
-            lat = self.latitude + other.get_lat()
+            lng = self.__longitude__ + other.get_long()
+            lat = self.__latitude__ + other.get_lat()
             return LatLong(lat, lng)
